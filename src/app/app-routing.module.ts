2a790d09eb91e99fd056d549aca9a0e5e174e65b
Sanya Oluwadare <sanyaodare@gmail.com>
2020-07-26 21:25:27 +0100

    add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 const routes: Routes = [
-  { path: '', component: TodosComponent }
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -11,7 +11,7 @@ export class AddTodoComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): {
+  ngOnInit() {
   }
 
   onSubmit() {
